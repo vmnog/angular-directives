2a8cbcd0a2cacac45d1a74f7cad5461c873c67f3
Victor Nogueira <p51901224@brasilprev.com.br>
2021-09-23 10:58:19 -0300

    feat: hostlistener listening mouse events

--- src/app/better-highlight/better-hightlight.directive.ts
+++ src/app/better-highlight/better-hightlight.directive.ts
@@ -1,4 +1,10 @@
-import { Directive, ElementRef, OnInit, Renderer2 } from "@angular/core";
+import {
+  Directive,
+  ElementRef,
+  HostListener,
+  OnInit,
+  Renderer2,
+} from "@angular/core";
 
 @Directive({
   selector: "[appBetterHightlight]",
@@ -11,6 +17,22 @@ export class BetterHightlightDirective implements OnInit {
       this.elementRef.nativeElement,
       "background-color",
       "yellow"
+    );
+  }
+
+  @HostListener("mouseenter") mousehover(eventData: Event) {
+    this.renderer.setStyle(
+      this.elementRef.nativeElement,
+      "background-color",
+      "blue"
+    );
+  }
+
+  @HostListener("mouseleave") mouseleave(eventData: Event) {
+    this.renderer.setStyle(
+      this.elementRef.nativeElement,
+      "background-color",
+      "transparent"
     );
   }
 }
